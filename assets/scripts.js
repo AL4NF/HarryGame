925ce2e91d1cd443bd13090ca4ad052171a4df88
Alan Fiscal <alanfiscalcampos@gmail.com>
2018-12-07 17:44:16 -0600

    Final

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,2 +1,6 @@
-var audio=getElementByTag("audio");
-audio.play();
+const intro =document.querySelector(".intro");
+const audio= document.querySelector("audio");
+
+intro.addEventListener("mouseover",function(){
+  audio.play();
+});
